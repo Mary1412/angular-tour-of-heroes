04ba6ac462340a5fc2d5468a73a24a2bf8043903
Mary Novikova <nmvikova44@gmail.com>
2022-02-18 20:09:25 +0300

    Create list of heroes with Editor

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./heroes/hero";
+
+export const HEROES: Hero[] =[
+    {id: 11, name: 'H1'},
+    {id: 12, name: 'H2'},
+    {id: 13, name: 'H3'},
+    {id: 14, name: 'H4'},
+    {id: 15, name: 'H5'},
+    {id: 16, name: 'H6'},
+    {id: 17, name: 'H7'},
+    {id: 18, name: 'H8'},
+    {id: 19, name: 'H9'},
+    {id: 20, name: 'H10'}
+];
